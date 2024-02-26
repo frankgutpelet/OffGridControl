9e90c2a16c5286d15c1baead3aa89114ebb72d63
frankgutpelet <frank.gutpelet@gmx.de>
2024-02-26 12:34:03 +0100

    pythonpath

--- web/startService.sh
+++ web/startService.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH=/home/frank/projects/SolarWatcher/web/
+export PYTHONPATH=/home/frank/projects/OffGridControl/web/
 ipaddress=`ifconfig wlan0 |grep netmask | sed 's/\s\s*/ /g' |cut -d ' ' -f3`
 python3 manage.py runserver $ipaddress:8000 > log/Django.log 2>&1&
